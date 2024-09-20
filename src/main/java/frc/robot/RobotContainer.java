2f8d71d024a39f82fe8d4f0ecb5aa84a55ffc982
lirpatankdrivehater <160079191+lilovexer@users.noreply.github.com>
2024-09-19 18:57:07 -0700

    Tankdrivefinishedalmost

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,11 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.TankDriveCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -20,16 +21,17 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DriveSubsystem m_exampleSubsystem = new DriveSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final XboxController m_driverController = 
+      new XboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    configureDefaultCommand();
   }
 
   /**
@@ -43,21 +45,23 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    
   }
 
+  private void configureDefaultCommand() {
+    
+    m_exampleSubsystem.setDefaultCommand(new TankDriveCommand(m_driverController, m_exampleSubsystem));
+
+  }
+  
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    
+   return new SequentialCommandGroup();
+    
   }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,6 +4,9 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -12,13 +15,40 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class DriveSubsystem extends SubsystemBase {
 
-  private CANSparkMax motorFrontLeft = new CANSparkMax(Constants.OperatorConstants.motorFrontLeft, MotorType.kBrushless);
-  private CANSparkMax motorFrontRight = new CANSparkMax(Constants.OperatorConstants.motorFrontRight, MotorType.kBrushless);
+  private CANSparkMax motorFrontLeft = new CANSparkMax(Constants.OperatorConstants.motorFrontLeft,
+      MotorType.kBrushless);
+  private CANSparkMax motorFrontRight = new CANSparkMax(Constants.OperatorConstants.motorFrontRight,
+      MotorType.kBrushless);
   private CANSparkMax motorBackLeft = new CANSparkMax(Constants.OperatorConstants.motorBackLeft, MotorType.kBrushless);
-  private CANSparkMax motorBackRight = new CANSparkMax(Constants.OperatorConstants.motorBackRight, MotorType.kBrushless);
+  private CANSparkMax motorBackRight = new CANSparkMax(Constants.OperatorConstants.motorBackRight,
+      MotorType.kBrushless);
+
+  private MotorControllerGroup left = new MotorControllerGroup(motorFrontLeft);
+  private MotorControllerGroup right = new MotorControllerGroup(motorFrontRight);
+
+  private DifferentialDrive drive = new DifferentialDrive(left, right);
 
   /** Creates a new DriveSubsystem. */
-  public DriveSubsystem() {}
+  public DriveSubsystem() {
+
+    motorFrontLeft.restoreFactoryDefaults();
+    motorFrontRight.restoreFactoryDefaults();
+    motorBackLeft.restoreFactoryDefaults();
+    motorBackRight.restoreFactoryDefaults();
+
+    motorFrontLeft.setInverted(false);
+    motorFrontRight.setInverted(true);
+
+    motorBackLeft.follow(motorFrontLeft);
+    motorBackRight.follow(motorFrontRight);
+
+  }
+
+public void definitelyNotTankDrive(double leftSpeed, double rightSpeed) {
+
+drive.tankDrive(leftSpeed, rightSpeed);
+
+}
 
   @Override
   public void periodic() {
