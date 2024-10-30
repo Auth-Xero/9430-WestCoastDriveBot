bfe9afe6d033a6f52d8e70e15d612b5b3b9d9108
IamIisI <titusj915@gmail.com>
2024-10-29 18:51:14 -0700

    Add arducam instances to BetterPhotonVision
    The names for the arducam cameras will be "Arducam_" + the index of the arducam

--- src/main/java/frc/robot/util/BetterPhotonVision.java
+++ src/main/java/frc/robot/util/BetterPhotonVision.java
@@ -1,8 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.util;
 
-/** Add your docs here. */
-public class BetterPhotonVision {}
+import org.photonvision.PhotonCamera;
+
+public class BetterPhotonVision {
+
+    PhotonCamera arducam1;
+    PhotonCamera arducam2;
+
+    public BetterPhotonVision() {
+
+        arducam1 = new PhotonCamera("Arducam_1");
+        arducam2 = new PhotonCamera("Arducam_2");
+    }
+
+}
